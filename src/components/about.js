9b3d6b9a67b17f686a4271e92e781b82d7839a2e
Allen McAfee <allen@robo3d.com>
2019-03-29 10:20:38 -0700

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -11,28 +11,35 @@ class About extends Component {
 				<Header />
 			<div className="row" id="featured-2">
 				  <div className="col-sm-8">
-				    <h2>Welcome to the Caltech ETDB, the blockchain-based Web 3.0 database of thousands of electron tomograms. </h2>
+				    <h2>Welcome to 3DRM, the blockchain-based Web 3.0 database of 3D artifacts. </h2>
 				  </div>
 				</div>
                 <div id="space50"></div>
 				<div className="row aboutintro" id="about">
-          <div className="col-sm-12">	
-						<h3>About Electron tomography</h3>
+          <div className="col-sm-12">
+						<h3>About 3DRM</h3>
 				    <div id="reddiv"></div>
           </div>
-				  <div className="col-sm-6">				      
-            <p>Electron tomography is a form of transmission electron microscopy in which a sample is tilted during imaging, typically one or two degrees between images, building up a "tilt-series" of images that can be computationally reconstructed into a 3D view of the sample, or "tomogram." In essence, electron tomography takes a CT scan of a cell.</p>
+				  <div className="col-sm-6">
+            <p>3DRM is a publishing platform tailored for 3D content, made for the content-addressable web.  With it you can protect, publish, and even monetize your work anywhere on the web.</p>
 					</div>
-				  <div className="col-sm-6"> 
-            <p>Currently, the majority of tomograms in the ETDB were acquired by cryo-EM, in which cells in standard growth media are plunged into a cryogen that freezes them so rapidly that the water molecules don't have a chance to rearrange into a crystalline lattice, preserving cell structures in vitreous, or glass-like, amorphous ice.  Vitrified cells can be imaged directly by electron tomography, without the need for fixation, dehydration, resin-embedding or staining.  This captures the cells in an essentially native state.</p>
+				  <div className="col-sm-6">
+            <p>Safely publish your work to the Interplanetary File System, then secure your terms and process payments via blockchain.</p>
           </div>
 				</div>
 				<div className="row" id="about">
 				  <div className="col-sm-6">
 				    <div>
-				      <h3>By making this data available, we hope others will use it to fuel their own discoveries.  </h3>
+				      <h3>All the tools you need to authenticate, publish, and license your 3D files.</h3>
 				      <div id="reddiv"></div>
-				      <p>In addition to its value for cell biologists, the ETDB offers a massive dataset for software developers to develop, test and improve image processing algorithms. We have some ideas for areas particularly ripe for improvement; check them out on the Scientific Challenges page.</p>
+				      <p>I3DRM is not a marketplace, so you keep more of your revenue.  It’s not a storage center, so we don’t own your files.  It’s not a walled-garden, so you’re free to host and sell anywhere.
+							<p>So what is it?  It’s a publishing platform you can use to:</p>
+							<ls>
+								<li>Set licensing terms how you want.  Use open source licenses, or charge a fee.  It’s up to you.</li>
+								<li>Notarize your license, download location, and more using blockchain.  Once published, your terms cannot be altered or destroyed.</li>
+								<li>Host your files on the Interplanetary Filing System.  Make use of distributed storage to keep your files away from centralized control.</li>
+								<li>A useful first step to copyrighting your work. Licensed  files can be used by the US Copyright office as proof of published work.</li>
+							</ls>
              <a className="browse" href="/challenges">View challenges <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
 				    </div>
 				  </div>
@@ -42,14 +49,14 @@ class About extends Component {
 				</div>
 				<div className="row aboutintro" id="about">
 				  <div className="col-sm-6">
-              <h3>Built with Blockchain</h3>			  		 
+              <h3>Built with Blockchain</h3>
 				      <div id="reddiv"></div>
 							<p>ETDB is an experiment in distributing large scientific datasets using the OIP specification. The addresses of the tomograms, and their corresponding metadata, are stored in the public FLO blockchain and the tomogram files are stored in the IPFS network. While etdb.caltech.edu offers one way to access the database, users are free to create their own applications to retrieve and interact with the data.</p>
-							
+
 							<p> A blockchain uses peer-to-peer networking to achieve network consensus without central points of failure, making it a public digital ledger which anyone can contribute to or audit, and no one can alter.</p>
 					</div>
 				  <div className="col-sm-6">
-				    <div>      
+				    <div>
 							<p>The <a target="blank" href="https://ipfs.io/">Inter Planetary File System (IPFS)</a> is a file transport protocol and network designed to create a content-addressable, peer-to-peer method of storing and sharing content in a distributed file system.</p>
 
 							<p>The <a target="blank" href="https://oip.wiki/Main_Page">Open Index Protocol (OIP)</a> specification defines standard use of these technologies; it uses the <a target="blank" href="https://flo.cash">FLO blockchain</a> to index metadata and IPFS to store files. The OIP system includes a transparent index, persistent file storage and permissionless access. The result is a robust, decentralized database open to anyone through open-source software.</p>
@@ -66,10 +73,10 @@ class About extends Component {
 							<p>If you are interested in learning more about electron tomography, this database, or other research conducted by the Jensen Lab please <a href="/contact">get in touch</a></p>
 							<p>Many of these datasets have been used in studies resulting in scientific publications. You can view a list of these publications on the <a target="blank" href="http://www.jensenlab.caltech.edu/publications">Jensen Lab</a> website.</p>
 							<a className="browse" href="http://www.jensenlab.caltech.edu">Jensen Lab <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
-					</div>			  		
+					</div>
 					</div>
 				</div>
-				<a name="FAQ"></a> 
+				<a name="FAQ"></a>
         <FAQ Core={this.props.Core} />
 				<Footer />
 			</div>
