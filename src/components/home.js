fc7eb9456eae2d1cf08d8b89ba6b04f61d8d8525
Allen McAfee <allen@robo3d.com>
2019-03-30 02:07:34 -0700

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -64,19 +64,20 @@ class Home extends Component {
 				      <h3>3D Digital Rights Management Database</h3>
 				      <h6>3DRM is a blockchain-protected publishing platform tailored for 3D content, made for the content-addressable web.  With it you can protect, publish, and even monetize your work anywhere on the web.</h6>
 				      <a className="browse" href="/browse">Browse database <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
-				   <a class="termsandconditions" href="/terms">View our terms and conditions</a>
-                    </div>
+				   		<a class="termsandconditions" href="/terms">View our terms and conditions</a>
+            </div>
 				  </div>
 				</div>
-			</div>
-			
-			<div id="home-image">
-				<div id="home-image-container"><img src={homePic} />
-				<p>SpaceX BFR Concept Rocket</p>
-				<p>IPFS Hash: QmWdeS1igSK82ytysQkkLg3qsr15c9seHmNXaCf1UxHAFU</p>
-				<a href="http://www.cloudflare-ipfs.com/ipfs/QmWdeS1igSK82ytysQkkLg3qsr15c9seHmNXaCf1UxHAFU"><p>Download from the Interplanetary Filing System</p></a>
-								</div>
-			</div>
+
+
+				<div id="home-image">
+					<div id="home-image-container"><img src={homePic} />
+						<p>SpaceX BFR Concept Rocket</p>
+						<p>IPFS Hash: QmWdeS1igSK82ytysQkkLg3qsr15c9seHmNXaCf1UxHAFU</p>
+						<a href="http://www.cloudflare-ipfs.com/ipfs/QmWdeS1igSK82ytysQkkLg3qsr15c9seHmNXaCf1UxHAFU"><p>Download from the Interplanetary Filing System</p></a>
+				 </div>
+				</div>
+		</div>
 		)
 	}
 }
